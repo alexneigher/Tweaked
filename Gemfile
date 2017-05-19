91676eda8fde4ba08b408503b6b47fb521d253df
Alex Neigher <alex.neigher@snapdocs.com>
2017-05-18 22:51:57 -0600

    add authentication

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
 
+gem 'devise'
+
 gem 'pg'
 gem 'haml'
 gem 'twitter-bootstrap-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'categories#index'
 
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  
   resources :categories, only: :show
 
   resources :titles do
