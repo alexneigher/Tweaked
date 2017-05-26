be63dcbbe2ec8b84d73c05aaf167c60995c01fd6
Alex Neigher <alex.neigher@snapdocs.com>
2017-05-25 22:12:26 -0600

    better functionality

--- app/assets/javascripts/tweak_builder.js
+++ app/assets/javascripts/tweak_builder.js
@@ -1,20 +1,49 @@
 $(function(){
 
-  $( "#draggable-tweak-characters" ).sortable();
+  //click on a letter to delete, this removes the "add letter container", shows the reset button
+  $('#sortable .deleteable').click(function(){
+    $(this).remove();
 
-  //drop in the delete area
-  $("#remove-character").droppable({
-    activeClass: 'active',
-    hoverClass:'hovered',
-    accept:".tweak-character",
-    drop:function(event, ui){
-      ui.draggable.remove();
-    }
+    //cannot delete any more letter
+    $('.deleteable').unbind('click').removeClass('deleteable');
+
+    //remove add letters button and show reset
+    $('.add-letter-btn').remove();
+    $('.reset-btn').removeClass('hidden');
+
+    //focus on description area
+    $('.description-area').removeClass('hidden').focus();
+
+    // show submit
+    $('.submit-button').removeClass('hidden');
   });
 
-  //click to add
-  $('.tweak-character.clickable').click(function(){
-    var $newChar = $(this).removeClass('clickable');
-    $('#draggable-tweak-characters').append( $newChar )
-  })
+  //click to add, this unbinds the ability to delete a letter
+  $('#addable-characters .tweak-character').click(function(){
+    $newchar = $(this).addClass('added').unbind('click');
+    $('#sortable').append( $newchar  );
+
+    //disable delete functionality
+    $('.deleteable').unbind('click').removeClass('deleteable');
+
+    //remove all other add letters
+    $('#alphabet-container').remove();
+
+    //remove add letters button and show reset
+    $('.add-letter-btn').remove();
+    $('.reset-btn').removeClass('hidden');
+
+    //focus on description area
+    $('.description-area').removeClass('hidden').focus();
+
+    // show submit
+    $('.submit-button').removeClass('hidden');
+  });
+
+
+  $( "#sortable" ).sortable({
+    cancel: ".not-draggable"
+  });
+
+  $( "#sortable .tweak-character" ).disableSelection();
 })
