53cc226b28f49d268a4dc28d95c4f0b35815ac00
Alex Neigher <alex.neigher@snapdocs.com>
2017-05-25 21:30:38 -0600

    old js

--- app/assets/javascripts/tweak_builder.js
+++ app/assets/javascripts/tweak_builder.js
@@ -1,35 +1,20 @@
 $(function(){
-  $('.tweak-character.clickable').click(function(){
-    $(this).remove();
 
-    $('.tweak-character').unbind('click').removeClass('clickable');
+  $( "#draggable-tweak-characters" ).sortable();
 
-    $('textarea').focus();
-
-    $('.submit-tweak').removeClass('disabled');
-
-    $('.reset-button').removeClass('hidden')
+  //drop in the delete area
+  $("#remove-character").droppable({
+    activeClass: 'active',
+    hoverClass:'hovered',
+    accept:".tweak-character",
+    drop:function(event, ui){
+      ui.draggable.remove();
+    }
   });
+
+  //click to add
+  $('.tweak-character.clickable').click(function(){
+    var $newChar = $(this).removeClass('clickable');
+    $('#draggable-tweak-characters').append( $newChar )
+  })
 })
-
-// No longer using drag and drop functionality
-// $(function(){
-
-//   $( "#draggable-tweak-characters" ).sortable();
-
-//   //drop in the delete area
-//   $("#remove-character").droppable({
-//     activeClass: 'active',
-//     hoverClass:'hovered',
-//     accept:".tweak-character",
-//     drop:function(event, ui){
-//       ui.draggable.remove();
-//     }
-//   });
-
-//   //click to add
-//   $('.tweak-character.clickable').click(function(){
-//     var $newChar = $(this).removeClass('clickable');
-//     $('#draggable-tweak-characters').append( $newChar )
-//   })
-// })
