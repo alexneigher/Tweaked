4ea5cbf8ad2253d64f101b591b6dafcc5bd91980
Alex Neigher <alex@borrowedandblue.com>
2017-07-09 09:57:04 -0700

    homepage change. add numers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,17 +3,23 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
 
-    @recent_descriptions = Description
-                      .includes(:user, tweak: :title)
-                      .order(upvotes: :desc)
-                      .where("created_at > ? ", 2.days.ago)
-                      .limit(9)
+    @highest_rated_descriptions_all_time = descriptions
+    @current_weeks_highest_rated_descriptions = descriptions.where("created_at > ?", 7.days.ago)
+    @most_recent_descriptions = descriptions.reorder(created_at: :desc)
   end
 
   def show
     @category = Category.find(params[:id])
     @titles = @category.titles.order(name: :asc)
   end
+
+  private
+    def descriptions
+        Description
+          .includes(:user, tweak: :title)
+          .order(upvotes: :desc)
+          .limit(9)
+    end
 
 
 end
