c197f1c0911a9281737d5ea0b85e21d3c1f71d96
Alex Neigher <alex@borrowedandblue.com>
2017-07-24 19:52:15 -0700

    email polish

--- app/mailers/daily_summary_mailer.rb
+++ app/mailers/daily_summary_mailer.rb
@@ -1,14 +1,15 @@
 class DailySummaryMailer < ApplicationMailer
 
-  def daily_summary_email(user, title, tweak, description, author, upvote_count)
+  def daily_summary_email(user, title, tweak, description, created_at, author, upvote_count)
     @title = title
     @tweak = tweak
     @description = description
-    @author = @author
+    @created_at = created_at
+    @author = author
     @upvote_count = upvote_count
     @user = user
-    mail(to: user.email, subject: "LETTERLOOSE Daily Wit-Stop: #{Date.current.strftime('%b %d, %Y')}")
+    mail(to: @user.email, subject: "LETTERLOOSE Daily Wit-Stop: #{Date.current.strftime('%b %d, %Y')}")
   end
 
-  #DailySummaryMailer.daily_summary_email(User.where(email: 'aneigher@gmail.com').last, "Tally Ho", "Tally Hoe", "Keeping track", "Rick", 10).deliver_now
+  #DailySummaryMailer.daily_summary_email(User.where(email: 'aneigher@gmail.com').last, "Tally Ho", "Tally Hoe", "Keeping track", Date.new(2017,2,2),"Rick", 10).deliver_now
 end
