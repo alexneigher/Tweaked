8891b863b7bad618eca0dd5ee10af94589d97394
Alex Neigher <alex@borrowedandblue.com>
2017-09-01 12:04:55 -0600

    password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  http_basic_authenticate_with name: "letterloose", password: "nougatcenter"
+
   protected
 
     def configure_permitted_parameters
